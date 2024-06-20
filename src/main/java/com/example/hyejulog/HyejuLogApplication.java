3943ec1ccdb21c67432287b656de98c37cfd6e6f
Hyeju Kim <kimoju03@naver.com>
2024-06-20 15:57:21 +0900

    Initial commit

---
+++ src/main/java/com/example/hyejulog/HyejuLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyejulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyejuLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyejuLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hyejulog/HyejuLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyejulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyejuLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
